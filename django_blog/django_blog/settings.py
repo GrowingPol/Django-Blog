340dd2bf166d24233d2f01af79616ce42fa04c2c
GrowingPol <growingpol@gmail.com>
2021-10-03 19:03:14 -0600

    Add crispyforms with bootstrap4 to a nicer form visualization and error feedback

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
 ]
@@ -126,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
