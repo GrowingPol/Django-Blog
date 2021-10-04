0d3e87a7d3f2a76d18edf800ef308c74cbff88a0
GrowingPol <growingpol@gmail.com>
2021-10-03 20:00:50 -0600

    define login/logout urls and create login template, and fill empty links of Sign In and Sign Up references

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -134,3 +134,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog-home' # redirect to home on user login
