26f2de1ef7eecdee7ae9ff18c1ada436447edd61
GrowingPol <growingpol@gmail.com>
2021-10-05 20:10:32 -0600

    Change media root and URL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #Full path to directory where django is going to store uploaded files
+MEDIA_URL = '/media/' #Where uploaded files is going to be located in the system
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
