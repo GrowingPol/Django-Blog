e8a47cbb483399051aa57bf0370af374c5b3836b
GrowingPol <growingpol@gmail.com>
2021-10-10 14:02:53 -0600

    Add LoginRequiredMixin to PostCreateView to avoid post new blogs without a signed-in profile

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView,
     DetailView,
@@ -23,7 +24,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView): #LoginRequiredMixin avoid to access this view without logging in
     model = Post
     fields = ['title', 'content']
 
