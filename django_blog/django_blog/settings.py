6cc8d754094f661826005e5bc6c511358690eb41
GrowingPol <growingpol@gmail.com>
2021-10-13 18:25:45 -0600

    Updated setting an vars

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$_v#ii)15g=xc%h1bt4lt-unjji#2!gr&my!6os+n9ugl9vtlr'
+#SECRET_KEY = 'django-insecure-$_v#ii)15g=xc%h1bt4lt-unjji#2!gr&my!6os+n9ugl9vtlr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mygrowingblog.herokuapp.com']
 
 
 # Application definition
