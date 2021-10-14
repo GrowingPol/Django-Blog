ffc4f7807b1583a763e2e720b7f24ba2d161b729
GrowingPol <growingpol@gmail.com>
2021-10-13 18:41:44 -0600

    Add Django-heroku settings module

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -164,3 +165,6 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'us-east-2'
+
+#Deployment in Heroku
+django_heroku.settings(locals())
